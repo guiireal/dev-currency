6d3f8a20ae8c1bfde645fc1c11dafaeed2a62113
guiireal <guii_real@hotmail.com>
2024-12-24 01:08:04 -0300

    wip

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import data from "../../data/coins.json";
 import { Coin } from "../../types";
+import { formatBRL, formatNumber } from "../../utils";
 import styles from "./styles.module.css";
 
 export default function Detail() {
   const { cripto } = useParams();
 
   const [coin, setCoin] = useState<Coin>();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
@@ -16,13 +18,50 @@ export default function Detail() {
           coin.symbol.toLocaleLowerCase() === cripto?.toLocaleLowerCase()
       );
 
+      if (!coinData) {
+        return;
+      }
+
+      coinData.price = formatBRL(coinData.price);
+      coinData.market_cap = formatBRL(coinData.market_cap);
+      coinData.delta_24h = formatNumber(coinData.delta_24h);
+
       setCoin(coinData);
+      setLoading(false);
     }, 500);
   }, [cripto]);
 
+  if (loading) {
+    return (
+      <div className={styles.container}>
+        <h4 className={styles.center}>Carregando informações...</h4>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.container}>
-      <h1>Detalhes: {cripto}</h1>
+      <h1 className={styles.center}>{coin?.name}</h1>
+      <p className={styles.center}>{coin?.symbol}</p>
+
+      <section className={styles.content}>
+        <p>
+          <strong>Preço: </strong> {coin?.price}
+        </p>
+        <p>
+          <strong>Valor de mercado: </strong> {coin?.market_cap}
+        </p>
+        <p>
+          <strong>Volume: </strong>{" "}
+          <span
+            className={
+              Number(coin?.delta_24h) >= 0 ? styles.tdProfit : styles.tdLoss
+            }
+          >
+            {coin?.delta_24h}
+          </span>
+        </p>
+      </section>
     </div>
   );
 }
--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,3 +1,11 @@
+import { Link } from "react-router";
+import styles from "./styles.module.css";
+
 export default function NotFound() {
-  return <h1>Página não existe</h1>;
+  return (
+    <div className={styles.container}>
+      <h1>Página não existe</h1>
+      <Link to="/">Acessar criptomoedas</Link>
+    </div>
+  );
 }
